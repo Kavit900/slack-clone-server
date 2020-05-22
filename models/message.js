d48f06839a25ad50644952684b8d416c466e5ec4
Kavit <kavitmht@gmail.com>
2020-05-21 18:05:21 -0700

    added message model

---
+++ models/message.js
@@ -0,0 +1,19 @@
+import { Sequelize, DataTypes } from "sequelize/types";
+
+export default (sequelize, DataTypes) => {
+    const Message = sequelize.define('message', {
+        text: DataTypes.STRING,
+    });
+
+    Message.associate = (models) => {
+        // 1:M
+        Message.belongsTo(models.Channel, {
+            foreignKey: 'channelId',
+        });
+        Message.belongsTo(models.User, {
+            foreignKey: 'userId',
+        });
+    };
+
+    return User;
+};
