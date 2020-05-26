9f14146f1485f93445690fa774a3d75a13a8c842
Kavit <kavitmht@gmail.com>
2020-05-25 22:00:34 -0700

    added all schemas to schema folder

--- index.js
+++ index.js
@@ -9,15 +9,13 @@ import path from 'path';
 import { fileLoader, mergeTypes, mergeResolvers } from 'merge-graphql-schemas';
 
 const app = express();
-const path = '/graphql';
-
+ 
 const types = fileLoader(path.join(__dirname, './schema')); 
 
 const typeDefs = mergeTypes(types);
 
-const resolvers = fileLoader(path.join(__dirname, './resolvers'));
+const resolvers = mergeResolvers(fileLoader(path.join(__dirname, './resolvers')));
 
-const resolvers = mergeResolvers(resolvers);
 
 const schema = makeExecutableSchema({
     typeDefs,
@@ -28,7 +26,9 @@ const server = new ApolloServer({ schema });
 
 app.use(path, bodyParser.json());
 
-server.applyMiddleware({ app, path });
+let pathGraphQl = '/graphql';
+
+server.applyMiddleware({ app, pathGraphQl });
 
  models.sequelize.sync({force: true}).then(() => {
   app.listen({ port: 8080 }, () =>
